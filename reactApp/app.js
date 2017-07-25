891713a4a120be15ba4479039222e2ae052f6688
Mason <lantay@alumni.unc.edu>
2017-07-25 09:16:20 -0700

    input box takes up whole screen

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,11 +1,14 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
+import MyEditor from './components/DraftJs';
 /* This can check if your electron app can communicate with your backend */
 // fetch('http://localhost:3000')
 // .then(resp => resp.text())
 // .then(text => console.log(text))
 // .catch(err => {throw err})
 
-ReactDOM.render(<p>React lives!</p>,
-   document.getElementById('root'));
+console.log('Myeditor', MyEditor);
+
+ReactDOM.render(
+  <MyEditor />,
+  document.getElementById('root'));
