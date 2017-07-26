ca541e9eeb752c562962c77b570f8eff1990fab2
Mason <lantay@alumni.unc.edu>
2017-07-26 14:40:17 -0700

    set up some of routes, WIP

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,20 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/docslist', function (req, res) {
+  var docsArray = [];
+  Document.find({collaborators:req.params.userId})
+    .exec()
+    .then((documents) => {
+      documents.forEach((document) => {
+        docsArray.push(document);
+      });
+    });
+  res.send(docsArray);
+
+});
+
+
 app.listen(3000, function () {
   console.log('Backend server for Electron App running on port 3000!');
 });
--- reactApp/components/Links.js
+++ reactApp/components/Links.js
@@ -1,32 +1,72 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Document } from './models';
-import mongoose from 'mongoose'; 
-// We will have to link to each document using a map and the MongoDB ids of each document. 
+import mongoose from 'mongoose';
+import axios from 'axios';import axios from 'axios';
 
-const Links = () => (
-  <div>
-    <input type="text" placeholder="Document Name" />
-    <input type="submit" value="Create New Document" />
-    <nav>
-      <ul>
-        <li><Link to='/Document1'>Document 1</Link></li>
+class Links extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      documents: [],
+      userId: 'string',
+    };
+  }
 
-          {/* Pending quinn's database work   */}
-         {Document.find()
-          .exec()
-          .then((documents) => {
-            documents.forEach((document) => {
-              <li><Link to={`/${document.name}`}>{document.name}</Link></li>;
-            });
-          })
-        } 
-        {/* end pending quinn's database work */}
-      </ul>
-    </nav>
-    <input type="text" placeholder="Paste Document ID" />
-    <input type="submit" value="Load Shared Document" />
-  </div>
+  componentDidMount() {
+    axios.get('/docslist'), {
+      params: {
+        userId: this.state.userId
+      }
+    }
+      .then(function (response) {
+        this.setState({
+          documents: response
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  
+  createDoc(){
+    axios.post('/createDoc'), {
+      params: {
+        userId: this.state.userId
+      }
+    }
+    .then(function (response) {
+      axios.get('/edit/:docId'), {
+        params: {
+          userId: this.state.userId
+        }
+      }  
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  }
+  
+  loadDoc(){
+
+  }
+  render() {
+    return (
+
+      <div>
+        <input type="text" placeholder="Document Name" />
+        <input type="submit" value="Create New Document" onSubmit={createDoc()}/>
+        <nav>
+          <ul>
+            <li><Link to='/Document1'>Document 1</Link></li>
+            {/* map over the documents in the state to get a list of the documents */}
+          </ul>
+        </nav>
+        <input type="text" placeholder="Paste Document ID" />
+        <input type="submit" value="Load Shared Document" onSubmit={loadDoc()} />
+      </div>
     );
 
-export default Links;
+  }
+  export default Links;
